382e7c8845fef031d43f2e875aa9fe6ae84a951a
munozlariosmarcos-ctrl <munozlariosmarcos@alumnos.ilerna.com>
2025-11-26 13:37:50 +0100

    Añadidos apartados en el Ej1.js

--- Entorno_Cliente/UA4/Ej1.js
+++ Entorno_Cliente/UA4/Ej1.js
@@ -1,7 +1,12 @@
 let persona={
     nombre:"Ana",
     edad:28,
-    trabajo:"Ingenieria"
+    trabajo:"Ingenieria",
+    //09.js
+    saludar: function() {
+        console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} años`);
+    }
+
 }
 
 console.log("Nombre :" , persona.nombre)
@@ -12,7 +17,7 @@ persona.pais="España"
 delete persona.trabajo
 
 console.log("Objeto completo : " , persona)
-
+//07.js
 let producto = "manzanas"
 
 let carrito={
@@ -24,7 +29,7 @@ let nuevoProducto = "peras";
 carrito[nuevoProducto] = 5;
 
 console.log("Carrito actualizado : " , carrito);
-
+//05.js
 if ("nombre" in persona) {
     console.log("La propiedad 'nombre' existe en persona.");
 } else {
@@ -36,7 +41,70 @@ if ("apellido" in persona) {
 } else {
   console.log("La propiedad 'apellido' NO existe en persona.");
 }
-
+//06.js
 for (let clave in persona){
     console.log("Objeto modificado " + clave + " : " + persona[clave])
 }
+//08.js
+const usuario2 = persona;
+
+usuario2.edad = 30;
+
+console.log("usuario1:", persona);
+console.log("usuario2:", usuario2);
+
+
+const clonUsuario = Object.assign({}, persona);
+clonUsuario.nombre = "Laura";
+
+console.log("usuario1:", persona);
+console.log("clonUsuario:", clonUsuario);
+
+persona.saludar();
+
+
+//010.js
+// Constructor Coche
+function Coche(marca, modelo, año) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.año = año;
+
+
+    this.detalles = function() {
+        console.log(`Coche: ${this.marca} ${this.modelo}, Año: ${this.año}`);
+    };
+}
+
+const coche1 = new Coche("Toyota", "Corolla", 2020);
+const coche2 = new Coche("Ford", "Focus", 2018);
+
+console.log(coche1);
+console.log(coche2);
+
+coche1.detalles();
+coche2.detalles();
+
+//011.js
+const id = Symbol("id");
+let empleado = {
+    nombre: "Carlos",
+    puesto: "Desarrollador"
+};
+
+empleado[id] = 12345;
+for (let clave in empleado) {
+    console.log(clave + " : " + empleado[clave]);
+}
+console.log("ID del empleado:", empleado[id]);
+
+//012.js
+let cuentaBancaria = {
+    saldo: 1000,
+
+    toString: function() {
+        return `Saldo: ${this.saldo} EUR`;
+    }
+};
+console.log(cuentaBancaria.toString());
+console.log(String(cuentaBancaria));  
