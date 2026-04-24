c44355076952480ad78446c1a49f27b860adc040
munozlariosmarcos-ctrl <munozlariosmarcos@alumnos.ilerna.com>
2026-04-24 11:51:49 +0200

    añado fromtemd

--- Entorno_servidor/steamlike_backend/steamlike_backend/urls.py
+++ Entorno_servidor/steamlike_backend/steamlike_backend/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/library/", include("library.urls")),
     path("api/auth/", include("accounts.urls")),
     path("api/users/", include("accounts.urls_users")),
-    path("api/catalog/", include("catalog.urls")),  
+    path("api/catalog/", include("catalog.urls")),
+    path("", TemplateView.as_view(template_name="index.html"), name="frontend"), 
 ]
